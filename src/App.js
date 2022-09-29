4a4ac32dd923f08f341f47d9ba1c8fa8e03efc8b
ewj224 <enochjo0623@gmail.com>
2022-09-29 01:28:57 -0400

    Clean file with router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import{
+  createBrowserRouter,
+  RouterProvider, 
+  Route,
+} from "react-router-dom";
+
 import './App.css';
+import Article from "./pages/article";
+import List from './pages/list';
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <List />,
+}]);
 
 function App() {
   return (
     <div className="App">
-      <header></header>
+      <RouterProvider router={router} />
     </div>
   );
 }
