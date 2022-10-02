263a1d2f8be74e161f28463cd695996eea148106
ewj224 <enochjo0623@gmail.com>
2022-10-02 10:57:26 -0400

    Working on Article Card and got data pushed in (31:35 most recent video) need to work on css

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useMemo} from "react";
 
-function ArticleCard({blurb, date, imageAlt, imageSrc, title}){
-    //make sure to go over link...
+function ArticleCard({blurb, date, id, imageAlt, imageSrc, title}){
+    const articleDate = useMemo(()=>{
+        if (!date) return '';
+        const parsedDate = new Date(date)
+        return parsedDate.toDateString();
+    }, [date]);
+    
     console.log(imageSrc);
     return(
         <div className="articleCard">
@@ -10,10 +15,10 @@ function ArticleCard({blurb, date, imageAlt, imageSrc, title}){
             </div>
             <div className="articleCard--text">
                 <h2>{title}</h2>
-                <p className="date">{date}</p>
+                <p className="date">{articleDate}</p>
                 <p className="blurb">{blurb}</p>
                 <p className="link">
-                    <a href="/article/${id}">Read More</a>
+                    <a href={`/article/${id}`}>Read More</a>
                 </p>
 
             </div>
--- src/pages/article.js
+++ src/pages/article.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useParams } from "react-router-dom";
+import Data from "../components/data";
 
 function Article(){
+    //maybe add background image?
+    const {id} = useParams();
+    
+    const articleData = Data.find((article)=>
+    article.id === id);
+
+    const date = useMemo(()=> {
+        if (!articleData)  return ''
+        const parsedDate = new Date(articleData.publishedDate)
+
+        return parsedDate.toDateString();}, 
+        [articleData]);
+    
     return(
-        <main className="class-one">
-            <h1>Article</h1>
+        // const backgroundImageURL = "url"
+        <main className="article">
+            <header
+                className="article-header"
+                style={{
+                    backgroundImage: `url('${articleData.image.url}')`, 
+                    backgroundPosition: 'center',
+                    backgroundSize: "cover",
+                    padding: "40px 25px",
+                }}
+            >
+                <div className="article--header--wrapper">
+                    <h1>{articleData.title}</h1>
+                    <p>{date}</p>
+                    <p className="blurb">{articleData.blurb}</p>
+                </div>
+            </header>
+            <section className="article--content">
+                {/* This is where content goes */}
+            </section>
         </main>
     )
 }
