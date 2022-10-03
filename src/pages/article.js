8ddb48449324f3e3395079d9419b011c5e5f3de5
ewj224 <enochjo0623@gmail.com>
2022-10-02 21:27:31 -0400

    Finished everything!

--- src/pages/article.js
+++ src/pages/article.js
@@ -35,7 +35,23 @@ function Article(){
                 </div>
             </header>
             <section className="article--content">
-                {/* This is where content goes */}
+                {articleData.articleText.map((text,i)=> {
+                    const type = text.type;
+                    switch(type){
+                        case "p":
+                            return <p key={i}>{text.data}</p>;
+                        case "h1":
+                            return <h1 key={i}>{text.data}</h1>;
+                        case "h2":
+                            return <h2 key={i}>{text.data}</h2>;
+                        case "h3":
+                            return <h3 key={i}>{text.data}</h3>;
+                        case "h4":
+                            return <h4 key={i}>{text.data}</h4>;
+                        default:
+                            return <p key={i}>{text.data}</p>;
+                    }
+                })}
             </section>
         </main>
     )
